bb4a3b9caf07de69bd3d8889eaec5a0e50de081d
jbardon <bardonjeremy1@gmail.com>
2018-01-08 21:13:43 +0100

    Split app and vendors for angularjs project

--- angularjs-app/gulp/tasks/browserify.js
+++ angularjs-app/gulp/tasks/browserify.js
@@ -1,22 +1,52 @@
 'use strict';
 
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
+
+var nodeResolve = require('resolve');
 
 /**
  * Regroupe tous les fichiers .js dans un seul fichier bundle.js
  * Permet d'utiliser le require de SystemJS
  */
-gulp.task('browserify', function () {
-	return browserify({
-			entries: ['./src/scripts/index.js'],
-			debug: true // pour générer la sourcemap
-		})
-		.bundle()
-		.pipe(source('bundle.js'))
-		.pipe(gulp.dest('./dist/'));
+gulp.task('browserify', function (cb) {
+	runSequence('bundle-app', 'bundle-vendors', cb);
+});
+
+var makeBundle = function (bundleType) {
+  var browerifyParams = {
+    debug: true // pour générer la sourcemap
+  };
+
+  if(bundleType === 'app') {
+    browerifyParams.entries = ['./src/scripts/index.js'];
+  }
+
+  var bundleConfig = browserify(browerifyParams);
+  var browserifyVendorFunction = (bundleType === 'app') ? bundleConfig.external.bind(bundleConfig) : bundleConfig.require.bind(bundleConfig);
+
+  // Read package.json and get dependencies' package ids
+  var packageManifest = require('../../package.json');
+  var packageIds = Object.keys(packageManifest.dependencies) || [];
+  packageIds.forEach(function (id) {
+    browserifyVendorFunction(nodeResolve.sync(id), {expose: id});
+  });
+
+  var bundleName = bundleType === 'app' ? 'bundle.js' : bundleType + '.js';
+  return bundleConfig.bundle()
+    .pipe(source(bundleName))
+    .pipe(gulp.dest('./dist'));
+};
+
+gulp.task('bundle-app', function () {
+  makeBundle('app');
+});
+
+gulp.task('bundle-vendors', function () {
+  makeBundle('vendors');
 });
 
 /**
