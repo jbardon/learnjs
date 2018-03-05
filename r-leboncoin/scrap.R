1902d4211f58c80de22e2e495768d12374fd8e2b
jeremy.bardon <jeremy.bardon@>
2018-03-05 16:34:12 +0100

    it works

--- r-leboncoin/scrap.R
+++ r-leboncoin/scrap.R
@@ -1,34 +1,81 @@
-library(rvest)
+library(rvest) # Scrapping
+library(readr) # Export CSV
+library(purrr) # Data manipulation
 
-get_offer_details <- function(link) {
-  offer_details_html <- read_html(link)
-  
-  description <- offer_details_html %>% html_nodes('[data-qa-id=adview_spotlight_description_container]')
-  title <- description %>% html_nodes('[data-qa-id=adview_title]') %>% html_text(trim = TRUE)
-  price <- description %>% html_nodes('[data-qa-id=adview_price] span') %>% html_text(trim = TRUE)
-  date <- description %>% html_nodes('[data-qa-id=adview_date]') %>% html_text(trim = TRUE)
-  surface <- offer_details_html %>% html_nodes('[data-qa-id=criteria_item_square] > div:nth-child(2)') %>% html_text(trim = TRUE)
-  dpe <-  offer_details_html %>% html_nodes('[data-qa-id=criteria_item_energy_rate] > div:nth-child(2)') %>% html_text(trim = TRUE)
-  ges <-  offer_details_html %>% html_nodes('[data-qa-id=criteria_item_ges] > div:nth-child(2)') %>% html_text(trim = TRUE)
-  
-  return(c(title, price, date, surface, dpe, ges))
+# vector containing character(0) are ignored
+safe_na <- function(value) {
+  return (ifelse(identical(value, character(0)), NA, value))
 }
 
-offers_list_html <- read_html('https://www.leboncoin.fr/ventes_immobilieres/offres/pays_de_la_loire/?th=1&location=Nantes%2044000%2CNantes%2CNantes%2044200&pe=4&sqs=0&sqe=2&ret=2')
-
-items <- offers_list_html %>% html_nodes('[itemtype$=Offer]')
-offers <- data.frame(title = character(0),
-                     price = numeric(0),
-                     date = character(0),
-                     surface = character(0),
-                     dpe = character(0),
-                     ges = character(0),
-                     stringsAsFactors = FALSE)
-
-for (i in seq_along(items)) {
-  offer <- items[i]
-  link <- offer %>% html_nodes('a') %>% html_attr('href')
-  full_link <- paste('http:', link, sep="")
-  toto <- c(get_offer_details(full_link), link)
-  offers[i,] <- toto
+get_offers_ids <- function() {
+  webpage <- read_html('https://www.leboncoin.fr/ventes_immobilieres/offres/pays_de_la_loire/?th=1&location=Nantes%2044000%2CNantes%2CNantes%2044200&pe=4&sqs=0&sqe=2&ret=2')
+  
+  offers <- 
+    webpage %>% html_nodes('[itemtype$=Offer]')
+  
+  get_offer_link <- function(offer) {
+    links <- 
+      offer %>% html_nodes('a') %>% 
+      html_attr('href')
+    
+    offers_ids <- 
+      links %>% sub(pattern = ".*/(.*).htm.*", replacement = "\\1", x = .)
+    
+    return(offers_ids)
+  }
+  
+  return(map_chr(offers, get_offer_link))
 }
+
+get_offer_details <- function(offer_id) {
+  offer_link <- paste(
+    "http://www.leboncoin.fr/ventes_immobilieres/", offer_id, ".htm?ca=18_s",
+    sep=""); 
+  
+  offer <- read_html(offer_link)
+  
+  description <- 
+    offer %>% html_nodes('[data-qa-id=adview_spotlight_description_container]')
+  
+  title <- 
+    description %>% html_nodes('[data-qa-id=adview_title]') %>% 
+    html_text(trim = TRUE)
+  
+  price <- 
+    description %>% html_nodes('[data-qa-id=adview_price] span') %>%
+    html_text(trim = TRUE) %>%
+    gsub(pattern="(\\s|\u20AC)", replacement="", x = .) %>% 
+    as.numeric()
+  
+  date <- 
+    description %>% html_nodes('[data-qa-id=adview_date]') %>% 
+    html_text(trim = TRUE) %>%
+    strptime(format="%d/%m/%Y à %kh%M", tz="Europe/Paris") %>%
+    as.POSIXct()
+  
+  surface <- 
+    offer %>% html_nodes('[data-qa-id=criteria_item_square] > div:nth-child(2)') %>% 
+    html_text(trim = TRUE) %>%
+    sub(pattern = "(.*)m²", replacement = "\\1", x = .) %>%
+    as.numeric()
+  
+  dpe <- 
+    offer %>% html_nodes('[data-qa-id=criteria_item_energy_rate] > div:nth-child(2)') %>%
+    html_text(trim = TRUE) %>%
+    sub(pattern = "([A-Z]).*", replacement = "\\1", x = .) %>%
+    safe_na
+  
+  ges <- offer %>% html_nodes('[data-qa-id=criteria_item_ges] > div:nth-child(2)') %>% 
+    html_text(trim = TRUE) %>%
+    sub(pattern = "([A-Z]).*", replacement = "\\1", x = .) %>%
+    safe_na
+  
+   tibble::tibble(
+     title = title, price = price, date = date, surface = surface,
+     dpe = dpe, ges = ges, link = offer_link)
+}
+
+offers_ids <- get_offers_ids()
+offers <- purrr::map_df(offers_ids, get_offer_details)
+
+write_csv(offers, "leboncoin.csv")
