f54336f9a80ea447668d9ba1bdb03c02b4121742
jbardon <bardonjeremy1@gmail.com>
2018-04-02 16:34:45 +0200

    Clean master branch

--- angular-app/src/app/app.cmp.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-    //moduleId: module.id,
-    selector: '#app',
-    template: require('./app.cmp.html')
-})
-export class App {
-}
--- angular-app/src/main.ts
+++
@@ -1,6 +0,0 @@
-import 'zone.js'; // Sinon angular ne marche pas...
-
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app/app.mod';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
